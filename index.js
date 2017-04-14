b6206d5af155c573db38f6f5cb083592aa4a4173
svenkatreddy <svenkatreddy@users.noreply.github.com>
2017-04-14 17:30:47 +0000

    Major Release 2.0

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const chai = require('chai');
 const chaiAsPromised = require("chai-as-promised");
 const sinonChai = require('sinon-chai');
 const dirtyChai = require('dirty-chai');
-const chaiEnzyme = require('chai-enzyme');
 
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 chai.use(dirtyChai);
-chai.use(chaiEnzyme());
 
 module.exports = chai;
